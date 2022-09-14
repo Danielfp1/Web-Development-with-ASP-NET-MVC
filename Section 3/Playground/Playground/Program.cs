0679425efd8a6ab8a7d6b45f12bc0bc41296da13
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-09-14 08:54:25 -0300

    Add Section 3 project

---
+++ Section 3/Playground/Playground/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyObject myObject = new MyObject(); 
+            MyObject myObject2 = new MyObject(10, 10);
+
+            
+
+
+            Console.WriteLine("object 1 property is " + myObject.Number3);
+            Console.WriteLine("object 2 property is " + myObject2.Number3);
+
+            Console.ReadLine();
+        }
+        class MyObject
+        {
+            int _number1 = 5;
+            int _number2 = 5;
+
+            public MyObject()
+            {
+                Number1 = 5;
+                Number2 = 5;
+            }
+            public MyObject(int number1, int number2)
+            {
+                Number1 = number1;
+                Number2 = number2;
+            }
+
+            public int Number1 { get => _number1; set => _number1 = value; }
+            public int Number2 { get => _number2; set => _number2 = value; }
+            public int Number3 { get { return _number1 + _number2; } }
+
+            public int Calculate(int number1, int number2)
+            {
+                return number1 + number2;
+            }
+            public int Calculate()
+            {
+                return _number1 + _number2;
+            }
+
+            public string MyMethod()
+            {
+                return "some value";
+            }
+        }
+    }
+}
---
+++ Section 3/Playground/Playground/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Playground")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Playground")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("5c44b9c3-633e-48ad-a0ee-8c7481c717c5")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
