2e493d38b844ed274bcb8121a7ae7f409841205e
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-09-23 11:47:39 -0300

    Update Section 3 with Polymorphism

--- Section 3/Playground/Playground/Program.cs
+++ Section 3/Playground/Playground/Program.cs
@@ -14,14 +14,20 @@ namespace Playground
             var customer = new MyLibrary.Customer();
             customer.UpdateName("Name");
 
+            
+            MyLibrary.ICalculate function = new MyLibrary.AddCalculate();
+
+            var resul = function.PerformeCal(10, 10);
+            Console.WriteLine(resul);
+
             MyObject myObject = new MyObject(); 
             MyObject myObject2 = new MyObject(10, 10);
 
             
 
 
-            Console.WriteLine("object 1 property is " + myObject.Number3);
-            Console.WriteLine("object 2 property is " + myObject2.Number3);
+            //Console.WriteLine("object 1 property is " + myObject.Number3);
+            //Console.WriteLine("object 2 property is " + myObject2.Number3);
 
             Console.ReadLine();
         }
