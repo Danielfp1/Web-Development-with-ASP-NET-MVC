74779f48fe0070da812463753cf5c42aeda0a276
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-09-27 10:11:31 -0300

    Update Section 5 with Model Validation

--- Section 5/HelloMVC/HelloMVC/Models/Customer.cs
+++ Section 5/HelloMVC/HelloMVC/Models/Customer.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace HelloMVC.Models
 {
     public class Customer
     {
         public String Id { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "Your string is too long!!")]
+        [DisplayName("Enter Your Name")]
         public string Name { get; set; }
         public string Telephone { get; set; }
     }
