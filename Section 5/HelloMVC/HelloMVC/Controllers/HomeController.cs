007b771350f59f461e920640446e857899acf86f
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-09-27 08:47:17 -0300

    Update Section 5 with Posting Models by Scaffolding (using cache)

--- Section 5/HelloMVC/HelloMVC/Controllers/HomeController.cs
+++ Section 5/HelloMVC/HelloMVC/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +10,23 @@ namespace HelloMVC.Controllers
 {
     public class HomeController : Controller
     {
+        ObjectCache cache = MemoryCache.Default;
+        List<Customer> customers;
+
+        public HomeController()
+        {
+            customers = cache["customers"] as List<Customer>;
+            if(customers == null)
+            {
+                customers = new List<Customer>();
+            }
+        }
+
+        public void SaveCache()
+        {
+            cache["customers"] = customers;
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -45,14 +63,19 @@ namespace HelloMVC.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer)
+        {
+            customer.Id = Guid.NewGuid().ToString();
+            customers.Add(customer);
+            SaveCache();
+
+            return RedirectToAction("CustomerList");
+        }
+
         public ActionResult CustomerList()
         {
-            List<Customer> customerList = new List<Customer>();
-
-            customerList.Add(new Customer() { Name = "Fred", Telephone = "123456" });
-            customerList.Add(new Customer() { Name = "Barney", Telephone = "098745" });
-
-            return View(customerList);
+            return View(customers);
         }
     }
 }
