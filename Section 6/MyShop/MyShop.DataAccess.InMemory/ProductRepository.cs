ab2780d4246f3a720513f2b7bb23def792f33e24
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-10-04 09:35:57 -0300

    Product Manager Pages Built and Test

--- Section 6/MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
+++ Section 6/MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -9,7 +9,7 @@ using System.Xml.Serialization;
 
 namespace MyShop.DataAccess.InMemory
 {
-    internal class ProductRepository
+    public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
         List<Product> products;
