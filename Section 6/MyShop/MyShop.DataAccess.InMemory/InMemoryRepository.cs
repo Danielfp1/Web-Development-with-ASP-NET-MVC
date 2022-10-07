eba6dfac968c389c3b69f1bab1e355863564bc83
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2022-10-07 16:00:50 -0300

    Added DI

--- Section 6/MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ Section 6/MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -10,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> where T : BaseEntity
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> items = new List<T>();
@@ -41,7 +42,7 @@ namespace MyShop.DataAccess.InMemory
         {
             T tToUpdate = items.Find(i => i.Id == t.Id);
 
-           if (tToUpdate != null)
+            if (tToUpdate != null)
             {
                 tToUpdate = t;
             }
@@ -63,7 +64,7 @@ namespace MyShop.DataAccess.InMemory
                 throw new Exception(className + " Not Found");
             }
         }
-        
+
         public IQueryable<T> Collection()
         {
             return items.AsQueryable();
